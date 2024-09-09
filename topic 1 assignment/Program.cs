b20e6a6a8a043266166d1cf5095485fd29295656
Horse Tranquilizer <animeberd9@gmail.com>
2024-09-09 08:40:25 -0400

    Update Program.cs

--- topic 1 assignment/Program.cs
+++ topic 1 assignment/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace topic_1_assignment
+namespace Topic_1_the_basics_of_printing_assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("    J           DDDDDDDD");
+            Console.WriteLine("    J           D        D");
+            Console.WriteLine("    J           D         D");
+            Console.WriteLine("    J           D         D");
+            Console.WriteLine("    J           D         D");
+            Console.WriteLine("    J           D         D");
+            Console.WriteLine("    J           D         D");
+            Console.WriteLine("    J           D         D");
+            Console.WriteLine("J   J           D         D");
+            Console.WriteLine("J   J           DDDDDDDDDD");
+            Console.WriteLine("  J");
+
         }
-    }
 }
