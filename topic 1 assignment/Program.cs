b1144d2ba5f62d94e59464941bfea425e88d85c2
Horse Tranquilizer <animeberd9@gmail.com>
2024-09-12 08:18:32 -0400

    Update Program.cs

--- topic 1 assignment/Program.cs
+++ topic 1 assignment/Program.cs
@@ -4,6 +4,9 @@ namespace Topic_1_the_basics_of_printing_assignment
     {
         static void Main(string[] args)
         {
+             Console.WriteLine("My name is John Doe")
+             Console.WriteLine("")
+             Console.WriteLine("Comments can be used to save code that isnt working for later development")
             Console.WriteLine("    J           DDDDDDDD");
             Console.WriteLine("    J           D        D");
             Console.WriteLine("    J           D         D");
